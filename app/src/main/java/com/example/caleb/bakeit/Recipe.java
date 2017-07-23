6ec039cc8b1b19ffc2c0d48603a2c10910b96a30
othesius-exe <othesius.exe@gmail.com>
2017-07-23 12:01:10 -0400

    First Commit

---
+++ app/src/main/java/com/example/caleb/bakeit/Recipe.java
@@ -0,0 +1,19 @@
+package com.example.caleb.bakeit;
+
+import java.util.List;
+
+/**
+ * Recipe Object
+ */
+
+public class Recipe {
+
+    // Recipe Components
+    // Instructions
+    private String mInstructions;
+
+    // Ingredients
+    private List<String> mIngredients;
+
+    //
+}
---
+++ app/src/main/java/com/example/caleb/bakeit/RecipeHolder.java
@@ -0,0 +1,15 @@
+package com.example.caleb.bakeit;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * ViewHolder for a Recipe Object
+ */
+
+public class RecipeHolder extends RecyclerView.ViewHolder {
+
+    public RecipeHolder(View view) {
+        super(view);
+    }
+}
