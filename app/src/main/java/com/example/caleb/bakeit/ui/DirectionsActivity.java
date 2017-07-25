b53aea640d8ad426afe95eae95e79dc1dca2347b
othesius-exe <othesius.exe@gmail.com>
2017-07-25 19:26:30 -0400

    Broke the intent again

--- app/src/main/java/com/example/caleb/bakeit/ui/DirectionsActivity.java
+++ app/src/main/java/com/example/caleb/bakeit/ui/DirectionsActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.example.caleb.bakeit.R;
@@ -25,6 +26,7 @@ import butterknife.ButterKnife;
 public class DirectionsActivity extends FragmentActivity {
 
     @BindView(R.id.recipe_title) TextView mRecipeTitle;
+    @BindView(R.id.recipe_recycler) RecyclerView mRecyclerView;
 
     // ViewPagerAdapter
     private RecipeInfoPagerAdapter mRecipeInfoPagerAdapter;
@@ -91,7 +93,9 @@ public class DirectionsActivity extends FragmentActivity {
         mDirectionsAdapter = new RecipeAdapter(this, mDirectionsObjects);
         mIngredientsAdapter = new RecipeAdapter(this, mIngredientObjects);
 
-        mRecipeInfoPagerAdapter = new RecipeInfoPagerAdapter(this, mSupportFragmentManager);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mDirectionsAdapter);
 
+        mRecipeInfoPagerAdapter = new RecipeInfoPagerAdapter(this, mSupportFragmentManager);
     }
 }
